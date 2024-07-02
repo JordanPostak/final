04a947eba729d5ebef64972a17d76ed028d93d78
JordanPostak <jordan_postak@hotmail.com>
2024-07-02 11:12:41 -0600

    getting passport back

--- server.js
+++ server.js
@@ -2,13 +2,15 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongodb = require('./data/database');
+const passport = require('passport');
 const session = require('express-session');
-const passport = require('passport'); // Remove GitHubStrategy import
+const GitHubStrategy = require('passport-github2').Strategy;
 const cors = require('cors');
-const usersRouter = require('./routes/users');
-const { hashPassword } = require('./utils/passwordUtils'); // Import your password hashing function
+const usersController = require('./controllers/users'); // Import users controller
 
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 app
@@ -22,12 +24,105 @@ app
     .use(passport.session())
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization');
-        res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, PATCH, OPTIONS, DELETE');
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization'
+        );
+        res.setHeader(
+            'Access-Control-Allow-Methods', 
+            'POST, GET, PUT, PATCH, OPTIONS, DELETE'
+        );
         next();
     })
-    .use(cors({
-        origin: ['http://localhost:3000', 'http://seerstoneapi.onrender.com', 'https://seerstoneapi.onrender.com'],
-        credentials: true
-    }))
-    .use("/", require("./routes/index.js")); // Assuming index.js routes to other endpoints
+    .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}))
+    .use(cors({ origin: ['http://localhost:3000', 'http://seerstoneapi.onrender.com', 'https://seerstoneapi.onrender.com'], credentials: true }))
+    .use("/", require("./routes/index.js"));
+
+   passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+},
+async function(accessToken, refreshToken, profile, done) {
+    try {
+        const usersCollection = mongodb.getDatabase().db('seerstone').collection('users');
+        const existingUser = await usersCollection.findOne({ user_id: profile.id });
+
+        if (existingUser) {
+            return done(null, existingUser);
+        } else {
+            let firstName = '';
+            let lastName = '';
+            if (profile.displayName) {
+                const nameParts = profile.displayName.split(' ');
+                firstName = nameParts[0];
+                lastName = nameParts[nameParts.length - 1];
+            }
+
+            const newUser = {
+                user_id: profile.id,
+                username: profile.username,
+                password: '', // Password is not provided by GitHub, handle it accordingly
+                first_name: firstName,
+                last_name: lastName,
+                email: (profile.emails && profile.emails[0] && profile.emails[0].value) || '', // Use the first email if available
+            };
+
+            await usersCollection.insertOne(newUser);
+            return done(null, newUser);
+        }
+    } catch (err) {
+        return done(err);
+    }
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user._id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const db = mongodb.getDatabase().db('seerstone');
+        const user = await db.collection('users').findOne({ _id: new mongodb.ObjectId(id) });
+        done(null, user);
+    } catch (error) {
+        done(error);
+    }
+});
+
+app.get('/', (req, res) => {
+    if (req.session.user !== undefined) {
+        const userid = req.session.user.user_id;
+        const displayName = req.session.user.username;
+        res.send(`
+            <h1>Logged in as ${displayName}</h1>
+            <h2>Use this to represent your user_id for collection fields in swagger: ${userid}</h2>
+            <h2><a href="https://seerstoneapi.onrender.com/api-docs">Click here to go to Swagger</a></h2>
+        `);
+    } else {
+        res.send(`
+        <h1>Logged Out</h1>
+    `);
+    }
+});
+
+app.get('/github/callback', passport.authenticate('github', {
+    failureRedirect: '/api-docs', session: false}),
+    (req, res) => {
+        req.session.user = req.user;
+        res.redirect('/');
+    });
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            if (process.env.NODE_ENV !== 'test') {
+                console.log(`Database is listening and node Running on port ${port}`);
+            }
+        });
+    }
+});
+
+module.exports = app;
