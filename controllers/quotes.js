25092fc07b8acef160ca7f0dd8433e0ced057cda
JordanPostak <jordan_postak@hotmail.com>
2024-07-13 22:01:16 -0600

    quotes controller mistake

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -7,7 +7,7 @@ const getAllQuotes = async (req, res) => {
     // #swagger.tags=['quotes']
     try {
         const result = await mongodb.getDatabase().db('seerstone').collection('quotes').find();
-        result.toArray().then((inspirations) => {
+        result.toArray().then((quotes) => {
             res.setHeader('Content-Type', 'application/json');
             res.status(200).json(quotes);
         });
