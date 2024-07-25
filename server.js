1c09c16a2a1874a8182f884de892002c1138cfa9
JordanPostak <jordan_postak@hotmail.com>
2024-07-25 17:39:46 -0600

    true is bulion

--- server.js
+++ server.js
@@ -2,23 +2,12 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongodb = require('./data/database');
 const session = require('express-session');
 const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-// CORS setup using the `cors` package
-app.use(cors({
-    origin: [
-        'http://localhost:5173',          // Local development
-        'https://seerstoneapi.onrender.com', // Your backend URL
-        'https://jordanpostak.github.io',  // GitHub Pages root URL
-        'https://jordanpostak.github.io/inspire-stone' // GitHub Pages specific project URL
-    ],
-    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
-    credentials: true
-}));
 
 // Middleware for setting CORS headers dynamically
 app.use((req, res, next) => {
@@ -27,19 +16,32 @@ app.use((req, res, next) => {
     next();
 });
 
+// CORS setup using the `cors` package
+app.use(cors({
+    origin: [
+        'http://localhost:5173',          // Local development
+        'http://seerstoneapi.onrender.com', // Your backend URL
+        'https://seerstoneapi.onrender.com', // Your backend URL
+        'https://jordanpostak.github.io',             // GitHub Pages root URL
+        'https://jordanpostak.github.io/inspire-stone' // GitHub Pages specific project URL
+    ],
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+    credentials: true
+}));
+
 // Other middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Session middleware
 app.use(session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        sameSite: 'None',
-        secure: true
-    }
+  secret: "secret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+      sameSite: 'None',
+      secure: true
+  }
 }));
 
 // Routes
