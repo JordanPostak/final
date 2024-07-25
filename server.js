126a4e993c4eeed39bae3c628f18f05950a3759f
JordanPostak <jordan_postak@hotmail.com>
2024-07-25 14:02:49 -0600

    cors manually

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const session = require('express-session');
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
