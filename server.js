a339d2467db9668e467f8f6c5df5380963ad7419
JordanPostak <jordan_postak@hotmail.com>
2024-07-25 10:08:54 -0600

    both github

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app
           'http://localhost:5173',          // Local development
           'http://seerstoneapi.onrender.com', // Your backend URL
           'https://seerstoneapi.onrender.com', // Your backend URL
-          'https://jordanpostak.github.io/inspire-stone' // GitHub Pages URL
+          'https://jordanpostak.github.io',             // GitHub Pages root URL
+          'https://jordanpostak.github.io/inspire-stone' // GitHub Pages specific project URL
       ],
       methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
       credentials: true,
