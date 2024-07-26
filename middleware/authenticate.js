9897ebabb36d63dbebc58b675ad8f3c18a00cdd5
JordanPostak <jordan_postak@hotmail.com>
2024-07-25 19:49:18 -0600

    isAuthenicated

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,11 +1,15 @@
 //...This is the middleware/authenticate.js file...
 
 const isAuthenticated = (req, res, next) => {
-    if (req.session && req.session.user) {
-      return next();
-    }
-    res.status(401).send('Unauthorized');
-  };
+  if (req.session && req.session.user) {
+    // Set cookie options
+    req.session.cookie.sameSite = 'None';
+    req.session.cookie.secure = true;
+    next();
+  } else {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+};
 
 module.exports = {
     isAuthenticated
