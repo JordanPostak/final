b4b3e320e61f6185ac0286c045773b313f5f71c4
JordanPostak <jordan_postak@hotmail.com>
2024-07-26 14:07:04 -0600

    cleaning up server.js

--- server.js
+++ server.js
@@ -9,42 +9,29 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// CORS setup using the `cors` package
-// app.use(cors());
+app.use(bodyParser.json({ limit: "25mb" }));
+app.use(cors());
 
-// Other middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", 
+    'http://localhost:5173',          // Local development
+    'http://seerstoneapi.onrender.com', // Your backend URL
+    'https://seerstoneapi.onrender.com', // Your backend URL
+    'https://jordanpostak.github.io',             // GitHub Pages root URL
+    'https://jordanpostak.github.io/inspire-stone' // GitHub Pages specific project URL
+  );
+  next();
+});
 
 // Session middleware
 app.use(session({
     secret: "secret",
     resave: false,
     saveUninitialized: true,
-    
 }));
 
 // Routes
 app.use("/", require("./routes/index.js"));
-
-app.get('/', (req, res) => {
-    if (req.session.user) {
-        const { user_id, username } = req.session.user;
-        const inspiration = req.session.inspiration;
-
-        res.send(`
-            <h1>Logged in as ${username}</h1>
-            <h2>User id: ${user_id}</h2>
-            ${inspiration ? `<h3>Currently viewing inspiration ID: ${inspiration._id}</h3>` : '<h3>Not viewing any inspiration</h3>'}
-            <h2><a href="https://seerstoneapi.onrender.com/api-docs">Click here to go to Swagger</a></h2>
-        `);
-    } else {
-        res.send(`
-            <h1>Logged Out</h1>
-            <h2><a href="https://seerstoneapi.onrender.com/api-docs">Click here to go to Swagger and register or login</a></h2>
-        `);
-    }
-});
 
 // Initialize MongoDB connection
 mongodb.initDb((err) => {
