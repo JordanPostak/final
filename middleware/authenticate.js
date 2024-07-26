b7bb1b8eee2501be670043c9b7bcd3e0fda18167
JordanPostak <jordan_postak@hotmail.com>
2024-07-26 11:36:37 -0600

    routes back to origional

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,10 +1,12 @@
-// middleware/authenticate.js
+//...This is the middleware/authenticate.js file...
 
 const isAuthenticated = (req, res, next) => {
-    if (req.session.user === undefined) {
+    if (req.session.user === undefined){
         return res.status(401).json("You do not have access.");
     }
     next();
 };
 
-module.exports = isAuthenticated;
+module.exports = {
+    isAuthenticated
+}
--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,29 @@
 //...This is the routes/users.js file...
 
+
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users');
-const isAuthenticated = require('../middleware/authenticate');
+const { isAuthenticated } = require('../middleware/authenticate');
 
 // Retrieve all user profiles
 router.get('/', usersController.getAllUsers);
+
 // Retrieve user profile information by ID
 router.get('/:id', usersController.getUserById);
+
 // Create a new user profile
 router.post('/register', usersController.registerUser);
+
 // Login user
 router.post('/login', usersController.loginUser);
+
 // Update user profile information by user ID
 router.put('/:id', isAuthenticated, usersController.updateUserById);
+
 // Delete user profile by user ID
 router.delete('/:id', isAuthenticated, usersController.deleteUserById);
+
 // Logout user
 router.post('/logout', usersController.logoutUser);
 
