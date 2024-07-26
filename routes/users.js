45efcf5c633ec51c138d43ad306add7a1b545f92
JordanPostak <jordan_postak@hotmail.com>
2024-07-26 11:23:33 -0600

    corsMiddleware before

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,20 @@ const usersController = require('../controllers/users');
 const isAuthenticated = require('../middleware/authenticate');
 const cors = require('cors');
 
+
+const corsMiddleware = cors({
+    origin: [
+      'http://localhost:5173',          // Local development
+      'http://seerstoneapi.onrender.com', // Your backend URL
+      'https://seerstoneapi.onrender.com', // Your backend URL
+      'https://jordanpostak.github.io',             // GitHub Pages root URL
+      'https://jordanpostak.github.io/inspire-stone' // GitHub Pages specific project URL
+    ],
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+    credentials: true,
+    maxAge: 600
+  });
+
 // Retrieve all user profiles
 router.get('/', usersController.getAllUsers);
 // Retrieve user profile information by ID
@@ -22,18 +36,3 @@ router.delete('/:id', isAuthenticated, usersController.deleteUserById);
 router.post('/logout', usersController.logoutUser);
 
 module.exports = router;
-
-
-
-const corsMiddleware = cors({
-  origin: [
-    'http://localhost:5173',          // Local development
-    'http://seerstoneapi.onrender.com', // Your backend URL
-    'https://seerstoneapi.onrender.com', // Your backend URL
-    'https://jordanpostak.github.io',             // GitHub Pages root URL
-    'https://jordanpostak.github.io/inspire-stone' // GitHub Pages specific project URL
-  ],
-  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
-  credentials: true,
-  maxAge: 600
-});
