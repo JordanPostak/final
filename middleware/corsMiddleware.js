18ce0b3f229da396a0ad0cb92ed39debfee4e602
JordanPostak <jordan_postak@hotmail.com>
2024-07-27 11:15:04 -0600

    calling cors

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -1,53 +1,41 @@
 // middleware/corsMiddleware.js
-const loginCors = (req, res, next) => {
-  const allowedOrigins = [
-    'http://localhost:5173',
-    'http://seerstoneapi.onrender.com',
-    'https://seerstoneapi.onrender.com',
-    'https://jordanpostak.github.io',
-    'https://jordanpostak.github.io/inspire-stone'
-  ];
 
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, UPDATE, PUT, PATCH');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+const cors = require('cors');
 
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(204);
-  } else {
-    next();
-  }
-};
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://seerstoneapi.onrender.com',
+  'https://seerstoneapi.onrender.com',
+  'https://jordanpostak.github.io',
+  'https://jordanpostak.github.io/inspire-stone'
+];
 
+// Configuration for routes that require credentials
+const loginCors = cors({
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
+});
 
-const simpleCors = (req, res, next) => {
-  const allowedOrigins = [
-    'http://localhost:5173',
-    'http://seerstoneapi.onrender.com',
-    'https://seerstoneapi.onrender.com',
-    'https://jordanpostak.github.io',
-    'https://jordanpostak.github.io/inspire-stone'
-  ];
-
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, UPDATE, PUT, PATCH');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(204);
-  } else {
-    next();
-  }
-};
+// Configuration for routes that don't require credentials
+const simpleCors = cors({
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+});
 
 module.exports = {
   loginCors,
