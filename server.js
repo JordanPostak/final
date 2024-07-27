67c1f2a63346b8b3c70e0a1597ec7c4d031edcb0
JordanPostak <jordan_postak@hotmail.com>
2024-07-26 18:26:12 -0600

    basic cors

--- server.js
+++ server.js
@@ -9,25 +9,26 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware for setting CORS headers dynamically
-app.use((req, res, next) => {
-    res.set('Access-Control-Allow-Origin', req.headers.origin);
-    res.set('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+// // Middleware for setting CORS headers dynamically
+// app.use((req, res, next) => {
+//     res.set('Access-Control-Allow-Origin', req.headers.origin);
+//     res.set('Access-Control-Allow-Credentials', 'true');
+//     next();
+// });
 
-// CORS setup using the `cors` package
-app.use(cors({
-    origin: [
-        'http://localhost:5173',          // Local development
-        'http://seerstoneapi.onrender.com', // Your backend URL
-        'https://seerstoneapi.onrender.com', // Your backend URL
-        'https://jordanpostak.github.io',             // GitHub Pages root URL
-        'https://jordanpostak.github.io/inspire-stone' // GitHub Pages specific project URL
-    ],
-    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
-    credentials: true
-}));
+app.use(cors());
+// // CORS setup using the `cors` package
+// app.use(cors({
+//     origin: [
+//         'http://localhost:5173',          // Local development
+//         'http://seerstoneapi.onrender.com', // Your backend URL
+//         'https://seerstoneapi.onrender.com', // Your backend URL
+//         'https://jordanpostak.github.io',             // GitHub Pages root URL
+//         'https://jordanpostak.github.io/inspire-stone' // GitHub Pages specific project URL
+//     ],
+//     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+//     credentials: true
+// }));
 
 // Other middleware
 app.use(bodyParser.json());
@@ -43,25 +44,6 @@ app.use(session({
 
 // Routes
 app.use("/", require("./routes/index.js"));
-
-app.get('/', (req, res) => {
-    if (req.session.user) {
-        const { user_id, username } = req.session.user;
-        const inspiration = req.session.inspiration;
-
-        res.send(`
-            <h1>Logged in as ${username}</h1>
-            <h2>User id: ${user_id}</h2>
-            ${inspiration ? `<h3>Currently viewing inspiration ID: ${inspiration._id}</h3>` : '<h3>Not viewing any inspiration</h3>'}
-            <h2><a href="https://seerstoneapi.onrender.com/api-docs">Click here to go to Swagger</a></h2>
-        `);
-    } else {
-        res.send(`
-            <h1>Logged Out</h1>
-            <h2><a href="https://seerstoneapi.onrender.com/api-docs">Click here to go to Swagger and register or login</a></h2>
-        `);
-    }
-});
 
 // Initialize MongoDB connection
 mongodb.initDb((err) => {
