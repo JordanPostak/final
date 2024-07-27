27b475dac44b31f1a1ffe8f0cf3629ecbeb94543
JordanPostak <jordan_postak@hotmail.com>
2024-07-27 15:51:45 -0600

    no session SameSite

--- server.js
+++ server.js
@@ -38,9 +38,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
     secret: "secret",
     resave: false,
-    saveUninitialized: true,
-    secure: true, // Ensure HTTPS is used
-    sameSite: 'None' // Required for cross-site cookies
+    saveUninitialized: true
 }));
 
 // Routes
